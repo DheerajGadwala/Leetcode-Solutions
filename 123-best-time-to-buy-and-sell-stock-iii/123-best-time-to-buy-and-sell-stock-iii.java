9cfc78bed34e38e347a6f8469ed21e0fed94673a
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-08 18:04:34 -0400

    Time: 1222 ms (5.03%), Space: 679.3 MB (5.14%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    Map<Integer, Map<Integer, Integer>> cache;
+    int[] prices;
+    
+    public int maxProfit(int[] prices) {
+        this.cache = new HashMap<>();
+        this.prices = prices;
+        return res(0, false, 2);
+    }
+    
+    public int res(int pos, boolean holding, int count) {
+        int key = pos * (holding ? 1 : -1);
+        if (!cache.containsKey(key)) {
+            cache.put(key, new HashMap<>());
+        }
+        if (cache.containsKey(key) && cache.get(key).containsKey(count)) {
+            return cache.get(key).get(count);
+        }
+        else if (pos == prices.length || (count == 0 && !holding)) {
+            return 0;
+        }
+        else if (holding) {
+            cache.get(key).put(count, Math.max(prices[pos] + res(pos+1, false, count), res(pos+1, true, count)));
+            return cache.get(key).get(count);
+        }
+        else {
+            cache.get(key).put(count, Math.max(-prices[pos] + res(pos+1, true, count-1), res(pos+1, false, count)));
+            return cache.get(key).get(count);
+        }
+    }
+}
