fad03a21fcb4c6285de2339f6bdc63af57c27805
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-11 22:10:25 -0400

    Time: 47 ms (5.08%), Space: 55.4 MB (5.23%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        
+        Arrays.sort(logs, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String[] aa = a.split(" ");
+                StringBuilder arem = new StringBuilder();
+                String[] ba = b.split(" ");
+                StringBuilder brem = new StringBuilder();
+                for (int i = 1; i < aa.length; i++) arem.append(aa[i]+" ");
+                for (int i = 1; i < ba.length; i++) brem.append(ba[i]+" ");
+                boolean adig = true, bdig = true;
+                for (char k: aa[1].toCharArray()) {
+                    if (k-48 < 0 || k-48>9) {
+                        adig = false;
+                        break;
+                    }
+                }
+                for (char k: ba[1].toCharArray()) {
+                    if (k-48 < 0 || k-48>9) {
+                        bdig = false;
+                        break;
+                    }
+                }
+                if (adig && bdig) {
+                    return 0;
+                }
+                else if (!adig && !bdig) {
+                    if (arem.toString().compareTo(brem.toString()) == 0) return aa[0].compareTo(ba[0]);
+                    else return arem.toString().compareTo(brem.toString());
+                }
+                else if (adig) {
+                    return 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+        });
+        
+        return logs;
+        
+    }
+}
