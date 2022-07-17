5028acb005178e9e6144df716d81b4c6ac588c2e
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-17 11:01:11 -0400

    Time: 1753 ms (5.01%), Space: 351.6 MB (5.38%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,38 +6,29 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preOrder: List[int], inOrder: List[int]) -> Optional[TreeNode]:
-        if len(preOrder) == 1:
-            return TreeNode(preOrder[0], None, None)
-        elif len(preOrder) == 0:
-            return None
-        else:
-            root = preOrder[0]
-            node = TreeNode(root, None, None)
-            inOrderLeft = []
-            inOrderLeftSet = set()
-            inOrderRight = []
-            inOrderRightSet = set()
-            preOrderLeft = []
-            preOrderRight = []
-            rightEnd = None
-            flag = False
-            for i in range(len(inOrder)):
-                if inOrder[i] == root:
-                    rightEnd = inOrder[i-1]
-                    flag = True
-                elif flag:
-                    inOrderRight.append(inOrder[i])
-                    inOrderRightSet.add(inOrder[i])
-                else:
-                    inOrderLeft.append(inOrder[i])
-                    inOrderLeftSet.add(inOrder[i])
-            for i in range(1, len(preOrder)):
-                if preOrder[i] in inOrderLeftSet:
-                    preOrderLeft.append(preOrder[i])
-                else:
-                    preOrderRight.append(preOrder[i])
-            node.left  = self.buildTree(preOrderLeft, inOrderLeft)
-            node.right = self.buildTree(preOrderRight, inOrderRight)
-            return node
-                    
         
+        def res(inorder = inOrder, preorder = preOrder):
+            if len(inorder) == 0:
+                return None
+            else:
+                n = preorder[0]
+                lIn = []
+                rIn = []
+                for i in range(len(inorder)):
+                    if inorder[i] == n:
+                        lIn = inorder[:i]
+                        rIn = inorder[i+1:]
+                        break
+                lInSet = set(lIn)
+                rInSet = set(rIn)
+                lPre = []
+                rPre = []
+                for i in range(1, len(preorder)):
+                    if preorder[i] in lInSet:
+                        lPre.append(preorder[i])
+                    elif preorder[i] in rInSet:
+                        rPre.append(preorder[i])
+                t = TreeNode(n, res(lIn, lPre), res(rIn, rPre))
+                return t
+
+        return res()        
