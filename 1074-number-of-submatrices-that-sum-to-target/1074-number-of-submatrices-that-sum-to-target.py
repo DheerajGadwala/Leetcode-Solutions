bd5690211b64ab81400296bfd22a4cc5efb6aae5
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-18 12:48:39 -0400

    Time: 808 ms (93.84%), Space: 15 MB (47.39%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.py
@@ -1,6 +1,8 @@
 class Solution:
     def numSubmatrixSumTarget(self, mat: List[List[int]], target: int) -> int:
         
+        # O(n^3) => Similar to count of subarrays whose sum equals target
+        
         m = len(mat)
         n = len(mat[0])
         
@@ -23,3 +25,28 @@ class Solution:
                     else:
                         h[val] = 1
         return ans
+
+# first attempt: O(n^4) solution
+    
+# class Solution:
+#     def numSubmatrixSumTarget(self, mat: List[List[int]], target: int) -> int:
+        
+#         m = len(mat)
+#         n = len(mat[0])
+        
+#         dp = [[0 for i in range(n+1)] for j in range(m+1)]
+        
+#         for i in range(m):
+#             for j in range(n):
+#                 dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] - dp[i][j] + mat[i][j]
+        
+#         ans = 0
+        
+#         for i in range(1, m+1):
+#             for j in range(1, n+1):
+#                 for k in range(i):
+#                     for l in range(j):
+#                         if dp[i][j] - dp[k][j] - dp[i][l] + dp[k][l] == target:
+#                             ans += 1
+        
+#         return ans
