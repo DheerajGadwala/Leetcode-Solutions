c790747333a063d195d1dd4515ca06117917231b
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-26 07:31:27 -0400

    Time: 677 ms (64.21%), Space: 17.9 MB (65.82%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.py
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.py
@@ -6,33 +6,16 @@ class Solution:
         inc = False
         dec = False
         for i in range(1, len(arr)):
-            if inc:
-                if arr[i] < arr[i-1]:
-                    inc, dec = False, True
-                    curr += 1
-                elif arr[i] == arr[i-1]:
-                    inc, dec = False, False
-                    curr = 1
-                else:
-                    inc, dec = True, False
-                    curr = 2
-            elif dec:
-                if arr[i] > arr[i-1]:
-                    inc, dec = True, False
-                    curr += 1
-                elif arr[i] == arr[i-1]:
-                    inc, dec = False, False
-                    curr = 1
-                else:
-                    inc, dec = False, True
-                    curr = 2
+            nextInc = arr[i] > arr[i-1]
+            nextDec = arr[i] < arr[i-1]
+            if (inc and nextDec) or (dec and nextInc):
+                curr += 1
+            elif nextInc or nextDec:
+                curr = 2
             else:
-                inc = arr[i] > arr[i-1]
-                dec = arr[i] < arr[i-1]
-                if inc or dec:
-                    curr = 2
-                else:
-                    curr = 1
+                curr = 1
+            inc = nextInc
+            dec = nextDec
             mx = max(mx, curr)
         return mx
             
