bbcfed72ecb1cef7698527a398299e520854e84c
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-26 13:20:14 -0400

    Time: 195 ms (97.87%), Space: 18.1 MB (89.36%) - LeetHub

--- 1036-escape-a-large-maze/1036-escape-a-large-maze.py
+++ 1036-escape-a-large-maze/1036-escape-a-large-maze.py
@@ -2,20 +2,19 @@ import heapq
 class Solution:
     def isEscapePossible(self, blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
         
-        def bfs(s, t, blocked):
-            q = [s]
+        def dfs(s, t, blocked):
+            stk = [s]
             blocked.add(tuple(s))
-            while len(q) != 0:
-                x, y = q.pop()
+            while len(stk) != 0:
+                x, y = stk.pop()
                 if (abs(s[0]-x) + abs(s[1]-y) >= 200):
                     return True
                 neighbours = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
                 for n in neighbours:
                     if n[0] > -1 and n[0] < 1000000 and n[1] > -1 and n[1] < 1000000 and n not in blocked:
-                        q.append(n)
+                        stk.append(n)
                         blocked.add(n)
             return (t[0], t[1]) in blocked
         
         blocked = {(a[0], a[1]) for a in blocked}
-        return bfs(source, target, set(blocked)) and bfs(target, source, set(blocked))
-                
+        return dfs(source, target, set(blocked)) and dfs(target, source, set(blocked))
