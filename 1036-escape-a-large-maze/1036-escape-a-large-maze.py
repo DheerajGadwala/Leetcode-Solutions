94d5fe90c83d4304bda8a8059b93a47a5baef84b
Dheeraj Gadwala <65684290+DheerajGadwala@users.noreply.github.com>
2022-07-26 13:13:49 -0400

    Time: 209 ms (96.81%), Space: 18 MB (92.55%) - LeetHub

--- 1036-escape-a-large-maze/1036-escape-a-large-maze.py
+++ 1036-escape-a-large-maze/1036-escape-a-large-maze.py
@@ -7,14 +7,14 @@ class Solution:
             blocked.add(tuple(s))
             while len(q) != 0:
                 x, y = q.pop()
-                if (abs(s[0]-x) + abs(s[1]-y) >= 200) or ([x, y] == t):
+                if (abs(s[0]-x) + abs(s[1]-y) >= 200):
                     return True
                 neighbours = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
                 for n in neighbours:
                     if n[0] > -1 and n[0] < 1000000 and n[1] > -1 and n[1] < 1000000 and n not in blocked:
                         q.append(n)
                         blocked.add(n)
-            return False
+            return (t[0], t[1]) in blocked
         
         blocked = {(a[0], a[1]) for a in blocked}
         return bfs(source, target, set(blocked)) and bfs(target, source, set(blocked))
