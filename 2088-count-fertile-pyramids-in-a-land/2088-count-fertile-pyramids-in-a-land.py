2a779cc6dba4722986f0c0eb2d80cc27501ae63a
Dheeraj Gadwala <drgad24@gmail.com>
2022-07-29 14:08:10 -0400

    Time: 3450 ms (17.50%), Space: 114 MB (8.75%) - LeetHub

--- 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
+++ 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.py
@@ -3,34 +3,35 @@ class Solution:
         
         m, n = len(grid), len(grid[0])
         
-        ans, ansr = 0, 0
-        
         @cache
-        def res(i, j):
+        def countDown(i, j):
             nonlocal m, n
             if i + 1 < m and j - 1 > -1 and j + 1 < n and grid[i][j] == 1:
-                l = res(i+1, j-1)
-                b = res(i+1, j)
-                r = res(i+1, j+1)
-                return 1 + min(l, b, r)
+                left = countDown(i+1, j-1)
+                bottom = countDown(i+1, j)
+                right = countDown(i+1, j+1)
+                return 1 + min(left, bottom, right)
             else:
                 return grid[i][j]
         
         @cache
-        def resr(i, j):
+        def countUp(i, j):
             nonlocal m, n
             if i - 1 > -1 and j - 1 > -1 and j + 1 < n and grid[i][j] == 1:
-                l = resr(i-1, j-1)
-                b = resr(i-1, j)
-                r = resr(i-1, j+1)
-                return 1 + min(l, b, r)
+                left = countUp(i-1, j-1)
+                top = countUp(i-1, j)
+                right = countUp(i-1, j+1)
+                return 1 + min(left, top, right)
             else:
                 return grid[i][j]
             
+        ans = 0
+            
         for i in range(m):
             for j in range(n):
-                if res(i, j) != 0:
-                    ans += res(i, j) - 1
-                if resr(i, j) != 0:
-                    ans += resr(i, j) - 1
+                if countDown(i, j) != 0:
+                    ans += countDown(i, j) - 1
+                if countUp(i, j) != 0:
+                    ans += countUp(i, j) - 1
+
         return ans
