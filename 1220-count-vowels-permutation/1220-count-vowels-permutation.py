b3a9640c99737a02b8cc5a592709f9284094948d
Dheeraj Gadwala <drgad24@gmail.com>
2022-08-06 21:57:48 -0400

    Time: 1202 ms (27.06%), Space: 35.9 MB (44.01%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,16 +1,26 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         
+        # BFS
         adj = {'a' : 'e', 'e' : 'ai', 'i' : 'aeou', 'o' : 'iu', 'u' : 'a'}
+        q = [('a', 1), ('e', 1), ('i', 1), ('o', 1), ('u', 1)]
+        count = {k:1 for k in q}
+        cnt = 0
         MOD = 10**9 + 7
         
-        @cache
-        def res(pos, curr):
+        while len(q) != 0:
+            u = q.pop(0)
+            curr, pos = u
             if pos == n:
-                return 1
-            ret = 0
+                cnt += count[u]
+                cnt %= MOD
+                continue
             for c in adj[curr]:
-                ret = (ret + res(pos + 1, c)) % MOD
-            return ret
-        
-        return (sum([res(1, c) for c in adj])) % MOD
+                v = (c, pos + 1)
+                if v not in count:
+                    q.append((c, pos + 1))
+                    count[v] = count[u]
+                else:
+                    count[v] += count[u]
+                    count[v] %= MOD
+        return cnt
