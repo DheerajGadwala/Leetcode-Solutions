9802bd3d67ad0483794b13df621fe3a829245394
Dheeraj Gadwala <drgad24@gmail.com>
2022-08-11 21:05:02 -0400

    Time: 1134 ms (50.00%), Space: 26.4 MB (94.87%) - LeetHub

--- 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.py
+++ 1785-minimum-elements-to-add-to-form-a-given-sum/1785-minimum-elements-to-add-to-form-a-given-sum.py
@@ -1,7 +1,5 @@
 class Solution:
     def minElements(self, nums: List[int], limit: int, goal: int) -> int:
-        s = sum(nums)
-        tar = abs(goal - s)
-        return math.ceil(tar / limit) 
+        return math.ceil(abs(goal - sum(nums)) / limit) 
                 
         
