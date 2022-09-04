a89cb46b7aa5416b5e4908ea3f76c958d52727a8
Dheeraj Gadwala <drgad24@gmail.com>
2022-09-04 16:49:24 -0400

    Time: 29 ms (94.62%), Space: 60.6 MB (85.89%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,16 +1,19 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
         
-        // min Stack and x Stack
+        // min Queue [increaasing], max Queue [decreasing]
         Deque<Integer> minQ = new LinkedList<>(), maxQ = new LinkedList<>();
+        // first, last
         int f = 0, l = 0, ans = 0;
         
         while (f < nums.length) {
+            // maintaining the monotonic property
             while (minQ.size() > 0 && nums[minQ.peekLast()] > nums[f]) minQ.pollLast();
             while (maxQ.size() > 0 && nums[maxQ.peekLast()] < nums[f]) maxQ.pollLast();
             minQ.add(f);
             maxQ.add(f);
             f++;
+            // correcting the l pointer for the current f pointer
             while (nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > limit) {
                 if (maxQ.peekFirst() == l) {
                     maxQ.pollFirst();
