4e1e17a347b5c7e33ee88cd16f86351d16523e3a
Dheeraj Gadwala <drgad24@gmail.com>
2022-09-04 21:59:27 -0400

    Time: 68 ms (93.75%), Space: 118.9 MB (50.00%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
@@ -1,14 +1,19 @@
 class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
         
+        // monotonically decreasing queue
         Deque<Integer> q = new LinkedList<>();
+        // window first and last pointers
         int n = chargeTimes.length, f = 0, l = 0, ans = 0;
+        // window sum
         long currSum = 0;
         
         while (f < n) {
+            // maintain the monotonically decreasing property of the queue
             while (q.size() > 0 && chargeTimes[q.peekLast()] < chargeTimes[f]) q.pollLast();
             q.addLast(f);
             currSum += runningCosts[f++];
+            // adjust l pointer for the current f pointer
             while (f!=l && chargeTimes[q.peekFirst()]+(f-l)*currSum > budget) {
                 if (q.peekFirst() == l) q.pollFirst();
                 currSum -= runningCosts[l++];
