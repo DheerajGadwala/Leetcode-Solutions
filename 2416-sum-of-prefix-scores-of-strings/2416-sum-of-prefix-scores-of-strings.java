053df76be1d9e2c812bd6a890379784aba0fdaaa
Dheeraj Gadwala <drgad24@gmail.com>
2022-09-18 00:22:26 -0400

    Time: 1229 ms (20.00%), Space: 200.6 MB (60.00%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
@@ -1,30 +1,48 @@
-class Node:
-    def __init__(self):
-        self.children = [None] * 26
-        self.cnt = 0
-        
-class Solution:
-    def sumPrefixScores(self, words: List[str]) -> List[int]:
-        root = Node()
-        for word in words:
-            curr = root
-            for i in word:
-                ind = ord(i) - ord('a')
-                if curr.children[ind] is None:
-                    curr.children[ind] = Node()
-                curr = curr.children[ind]
-                curr.cnt += 1
-        
-        ret = []
-        for word in words:
-            curr = root
-            nxt = 0
-            for i in word:
-                ind = ord(i) - ord('a')
-                if curr.children[ind] is not None:
-                    curr = curr.children[ind]
-                    nxt += curr.cnt
-                else:
-                    break
-            ret.append(nxt)
-        return ret
+class Solution {
+    
+    // TrieNode
+    static class Node {
+        Map<Character, Node> children;
+        int count;
+        Node() {
+            children = new HashMap<>();
+            count = 0;
+        }
+        boolean contains(char c) {
+            return children.containsKey(c);
+        }
+    }
+    
+    public int[] sumPrefixScores(String[] words) {
+        Node root = new Node();
+        // add words to Trie and update counts
+        for(String word: words) {
+            Node curr = root;
+            for (char c: word.toCharArray()) {
+                if (!curr.contains(c)) {
+                    curr.children.put(c, new Node());
+                }
+                curr = curr.children.get(c);
+                curr.count += 1;
+            }
+        }
+        int[] ret = new int[words.length];
+        // find answer for the ith word
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            int cnt = 0;
+            Node curr = root;
+            for (char c: word.toCharArray()) {
+                if (curr.contains(c)) {
+                    curr = curr.children.get(c);
+                    cnt += curr.count;
+                }
+                else {
+                    break;
+                }
+            }
+            ret[i] = cnt;
+        }
+        return ret;
+    }
+}
