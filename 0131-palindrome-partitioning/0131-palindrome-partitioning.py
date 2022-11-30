999cee95e1f2b433008154ba658f4e7bfebb07d7
Dheeraj Gadwala <drgad24@gmail.com>
2022-11-30 14:52:28 -0500

    Time: 2123 ms (6.55%), Space: 29.6 MB (76.49%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, k: str) -> List[List[str]]:
+        
+        
+        def isPal(s):
+            i, j = 0, len(s) - 1
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        
+        def res(s = k):
+            if s == "":
+                return [[]]
+            n = len(s)
+            curr = ""
+            ret = []
+            for i in range(n):
+                curr += s[i]
+                if (isPal(curr)):
+                    rem = res(s[i+1:])
+                    for val in rem:
+                        ret.append([curr] + val)
+            return ret
+        
+        return res()
+                    
