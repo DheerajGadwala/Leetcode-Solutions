8a3dabb21084624e8d5146fdfb34d96c8cf01e83
Dheeraj Gadwala <drgad24@gmail.com>
2022-12-05 11:05:51 -0500

    Time: 1005 ms (90.21%), Space: 36.9 MB (27.71%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -2,12 +2,12 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         n = len(speed)
         sp = [(speed[i], position[i]) for i in range(n)]
-        sp.sort(key = lambda x: (x[1], -x[0]))
-        ans = 0
+        sp.sort(key = lambda x: x[1])
+        ans = 1
         while len(sp) > 1:
             s1, p1 = sp.pop()
             s2, p2 = sp.pop()
-            t = abs(math.inf if s1 >= s2 else (p1 - p2) / (s2 - s1))
+            t = math.inf if s1 >= s2 else (p1 - p2) / (s2 - s1)
             if s1 * t + p1 > target:
                 sp.append((s2, p2))
                 ans += 1
@@ -15,4 +15,4 @@ class Solution:
                 sp.append((s1, p1))
             elif s1 >= s2:
                 sp.append((s2, p2))
-        return ans + len(sp)
+        return ans
