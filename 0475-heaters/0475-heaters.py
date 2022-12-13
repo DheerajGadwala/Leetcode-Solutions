47ff9033d440e8a52d1adc697690961ee815f463
Dheeraj Gadwala <drgad24@gmail.com>
2022-12-13 10:31:00 -0500

    Time: 1264 ms (20.59%), Space: 17.5 MB (76.06%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        m, n = len(houses), len(heaters)
+        def search(val):
+            l, h = 0, n - 1
+            floor, ceil = 0, n-1
+            while l <= h:
+                mid = (l + h) // 2
+                if heaters[mid] == val:
+                    return mid, mid
+                elif heaters[mid] < val:
+                    floor = mid
+                    l = mid + 1
+                else:
+                    ceil = mid
+                    h = mid - 1
+            return floor, ceil
+        
+        radius = 0
+        
+        for i in houses:
+            f, c = search(i)
+            # print(i, heaters[f], heaters[c], min(abs(i - heaters[f]), abs(i - heaters[c])))
+            radius = max(radius, min(abs(i - heaters[f]), abs(i - heaters[c])))
+            
+        return radius
+        
