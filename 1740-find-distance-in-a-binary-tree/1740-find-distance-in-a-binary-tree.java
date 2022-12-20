ed0bb6485e99a3d07271e0712b36f8d476cc938d
Dheeraj Gadwala <drgad24@gmail.com>
2022-12-19 22:31:08 -0500

    Time: 2 ms (84.89%), Space: 51 MB (37.78%) - LeetHub

---
+++ 1740-find-distance-in-a-binary-tree/1740-find-distance-in-a-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ph = 0, qh = 0, ah = 0, p, q;
+    public int findDistance(TreeNode root, int p, int q) {
+        this.p = p;
+        this.q = q;
+        res(root, 0);
+        // System.out.println(ph + " " + qh + " " + ah);
+        return ph + qh - 2*ah;
+    }
+    private TreeNode res(TreeNode node, int h) {
+        if (node == null) {
+            return null;
+        }
+        else {
+            TreeNode left = res(node.left, h+1);
+            TreeNode right = res(node.right, h+1);
+            // System.out.println(left + " " + right + " " + node.val);
+            if (left != null && right != null) ah = h;
+            else if ((node.val == p || node.val == q) && (left != null || right != null)) ah = h;
+            else if (node.val == p && node.val == q) ah = h;
+            if (node.val == p) ph = h;
+            if (node.val == q) qh = h;
+            if (node.val == p || node.val == q) return node;
+            else if (left != null) return left;
+            else if (right != null) return right;
+            else return null;
+        }
+    }
+    
+    
+}
