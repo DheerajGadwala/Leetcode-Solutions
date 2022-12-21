ecda661ad4a3c5a3f464e91a71d2e751a0b84e7e
Dheeraj Gadwala <drgad24@gmail.com>
2022-12-21 10:00:31 -0500

    Time: 2402 ms (57.48%), Space: 14.9 MB (25.70%) - LeetHub

---
+++ 2473-minimum-cost-to-buy-apples/2473-minimum-cost-to-buy-apples.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minCost(self, n: int, roads: List[List[int]], appleCost: List[int], k: int) -> List[int]:
+        adj = {i:[] for i in range(n)}
+        
+        for u, v, w in roads:
+            adj[u-1].append((v-1, w*(k+1)))
+            adj[v-1].append((u-1, w*(k+1)))
+        
+        def bfs(src):
+            q = [(appleCost[src], src)]
+            cost = {src:0}
+            ans = math.inf
+            while len(q) != 0:
+                c, u = heappop(q)
+                ans = min(ans, c)
+                for v, w in adj[u]:
+                    if ((v in cost and cost[v] > cost[u] + w) or v not in cost) and cost[u] + w < c:
+                        heappush(q, (cost[u] + w + appleCost[v], v))
+                        cost[v] = cost[u] + w
+            return ans
+        
+        return [bfs(i) for i in range(n)]
