bcb1a98db2cfc910b7131e92746691a3e45064f1
Dheeraj Gadwala <drgad24@gmail.com>
2023-02-15 16:20:34 -0500

    Time: 134 ms (19.76%), Space: 14.7 MB (32.91%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        path = []
+        visited = dict()
+        adj = dict()
+        for u, v in tickets:
+            if u not in adj:
+                adj[u] = []
+            if v not in adj:
+                adj[v] = []
+            adj[u].append(v)
+            if u + v not in visited:
+                visited[u + v] = 0
+            visited[u+v] += 1
+        for u in adj:
+            adj[u].sort()
+        
+        ans = None
+        visCount = 1
+        
+        def res(u = "JFK"):
+            nonlocal ans, visCount
+            path.append(u)
+            # print(path, visCount, len(tickets) + 1)
+            if ans != None:
+                return
+            if visCount == len(tickets) + 1:
+                ans = path[:]
+                return
+            
+            for v in adj[u]:
+                if visited[u+v] > 0:
+                    visited[u + v] -= 1
+                    visCount += 1
+                    res(v)
+                    visited[u + v] += 1
+                    visCount -= 1
+            
+            path.pop()
+        
+        res()
+        return ans
