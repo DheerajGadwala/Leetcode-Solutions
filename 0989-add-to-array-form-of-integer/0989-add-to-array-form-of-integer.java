7692f4f26800d4ec253f9cb63e6c81e97f85f5de
Dheeraj Gadwala <drgad24@gmail.com>
2023-02-15 11:13:40 -0500

    Time: 5 ms (49.57%), Space: 44.1 MB (35.29%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -4,12 +4,20 @@ class Solution {
         int carry = 0, i = num.length - 1;
         while (i > -1 || k > 0 || carry > 0) {
             int val = k % 10 + carry + (i > -1 ? num[i] : 0);
-            ret.add(0, val % 10);
+            ret.add(val % 10);
             carry = val / 10;
             i--;
             k /= 10;
         }
         if (carry != 0) ret.add(0, carry);
+        int t = 0, j = ret.size() - 1;
+        while (t < j) {
+            ret.set(t, ret.get(t) + ret.get(j));
+            ret.set(j, ret.get(t) - ret.get(j));
+            ret.set(t, ret.get(t) - ret.get(j));
+            t++;
+            j--;
+        }
         return ret;
     }
 }
