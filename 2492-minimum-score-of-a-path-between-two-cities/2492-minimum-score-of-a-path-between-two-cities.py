9ee0b20f8b884166aef07a387b282b50aaa20645
Dheeraj Gadwala <drgad24@gmail.com>
2023-03-22 12:51:29 -0400

    Time: 2247 ms (32.88%), Space: 82 MB (12.42%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = dict()
+        for i in range(1, n + 1):
+            adj[i] = dict()
+        for u, v, d in roads:
+            adj[u][v] = d
+            adj[v][u] = d
+        q = [1]
+        minEdge = math.inf
+        visited = {1, }
+        while len(q) != 0:
+            u = q.pop(0)
+            for v in adj[u]:
+                if v not in visited:
+                    visited.add(v)
+                    q.append(v)
+                minEdge = min(minEdge, adj[u][v])
+        return -1 if n not in visited else minEdge
+        
