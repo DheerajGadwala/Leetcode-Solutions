cf4963de5af5d8051987e9b64bb0a7959c81b5a1
Dheeraj Gadwala <drgad24@gmail.com>
2023-05-02 12:58:28 -0400

    Time: 70 ms (13.74%), Space: 16.6 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -4,6 +4,6 @@ class Solution:
         for n in nums:
             if n == 0:
                 ans = 0
-            else:
-                ans *= n // abs(n)
+            elif n < 0:
+                ans *= -1
         return ans
