0900ecfa78c26ff5f03a116305ff0c2d11729e10
Dheeraj Gadwala <drgad24@gmail.com>
2023-05-31 20:29:57 -0400

    Time: 7 ms (51.09%), Space: 43.9 MB (5.43%) - LeetHub

---
+++ 2638-count-the-number-of-k-free-subsets/2638-count-the-number-of-k-free-subsets.java
@@ -0,0 +1,35 @@
+class Solution {
+    public long countTheNumOfKFreeSubsets(int[] nums, int k) {
+        Arrays.sort(nums);
+        List<Integer>[] lists = new ArrayList[k];
+        long ans = 1;
+        for (int i = 0; i < k; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        for (int n: nums) {
+            lists[n % k].add(n);
+        }
+        for (int i = 0; i < k; i++) {
+            long a = 1, b = 2;
+            List<Integer> list = lists[i];
+            int m = list.size();
+            if (m == 0) continue;
+            // System.out.println(list);
+            for(int j = m - 2; j > -1; j--) {
+                if (list.get(j) + k == list.get(j + 1)) {
+                    long c = a + b;
+                    a = b;
+                    b = c;
+                }
+                else {
+                    a = b;
+                    b *= 2;
+                }
+                // System.out.println(j + " " + b + " " + a);
+            }
+            ans *= b;
+        }
+        return ans;
+    }
+    
+}
