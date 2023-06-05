b7afc4a59160f511caace54ebc3beea6f9d77ae4
Dheeraj Gadwala <drgad24@gmail.com>
2023-06-05 12:35:08 -0400

    Time: 0 ms (100.00%), Space: 43 MB (17.75%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean checkStraightLine(int[][] coors) {
         if (coors[1][0] == coors[0][0]) {
-            boolean ret = true;
             for (int[] v: coors) {
-                ret &= v[0] == coors[1][0];
+                if (v[0] != coors[1][0])
+                    return false;
             }
-            return ret;
+            return true;
         }
         double m = 1.0*(coors[1][1] - coors[0][1]) / (coors[1][0] - coors[0][0]);
         double c = coors[0][1] - coors[0][0] * m;
-        boolean ret = true;
         for (int[] v: coors) {
-            ret &= v[1] == m * v[0] + c;
+            if (v[1] != m * v[0] + c)
+                return false;
         }
-        return ret;
+        return true;
     }
 }
