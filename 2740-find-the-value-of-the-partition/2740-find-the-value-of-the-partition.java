7c3ef55a19904144e1f331d2df5ac951ebac5f7f
Dheeraj Gadwala <drgad24@gmail.com>
2023-06-22 15:29:43 -0400

    Time: 22 ms (100.00%), Space: 54.5 MB (75.80%) - LeetHub

---
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findValueOfPartition(int[] nums) {
+        Arrays.sort(nums);
+        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i < nums.length; i++)
+            ans = Math.min(ans, nums[i] - nums[i-1]);
+        return ans;
+    }
+}
