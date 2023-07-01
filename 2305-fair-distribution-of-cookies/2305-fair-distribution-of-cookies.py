a1f48b3486da025c5265d1c933032ed0882613e5
Dheeraj Gadwala <drgad24@gmail.com>
2023-07-01 14:25:54 -0400

    Time: 54 ms (90.30%), Space: 16.4 MB (21.27%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,23 +1,23 @@
-class Solution:
-    def distributeCookies(self, cookies: List[int], k: int) -> int:
-        ans = sum(cookies)
-        N = len(cookies)
-        total = [0]*k
+class Solution(object):
+    def distributeCookies(self, cookies, k):
+        self.ans = float('inf')
+        self.kids = [0] * k
         
-        def backtrack(i, total):
-            nonlocal ans
-            if i == N:
-                ans = min(ans, max(total))
+        def backtrack(i):
+            if i == len(cookies):
+                self.ans = min(self.ans, max(self.kids))
                 return
             
-            if max(total) >= ans:
-                return
-            
+            seen = set()
             for j in range(k):
-                total[j] += cookies[i]
-                backtrack(i+1, total)
-                total[j] -= cookies[i]
-        
-
-        backtrack(0, total)
-        return ans
+                if self.kids[j] in seen: # pruning condition 1
+                    continue
+                if self.kids[j] + cookies[i] > self.ans: # pruning condition 2
+                    continue
+                seen.add(self.kids[j])
+                self.kids[j] += cookies[i]
+                backtrack(i + 1)
+                self.kids[j] -= cookies[i]
+                
+        backtrack(0)
+        return self.ans
