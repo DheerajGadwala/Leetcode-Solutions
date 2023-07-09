ac45f679453755bdfafce9bc5a6a78fb586bfb55
Dheeraj Gadwala <drgad24@gmail.com>
2023-07-09 17:35:28 -0400

    Time: 3949 ms (56.41%), Space: 16.5 MB (25.64%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -0,0 +1,45 @@
+class Solution:
+    def largestVariance(self, s: str) -> int:
+        alphabets = 'qwertyuiopasdfghjklzxcvbnm'
+        alphabets = 'qwertyuiopasdfghjklzxcvbnm'
+        alphabets = 'abcdefghijklmnopqrstuvwxyz'
+        ans = 0
+        for i in alphabets:
+            for j in alphabets:
+                l = [0]
+                x, y = False, False
+                for k in s:
+                    if i == k:
+                        if l[-1] >= 0:
+                            l[-1] += 1
+                        else:
+                            l.append(1)
+                        x = True
+                    elif j == k:
+                        if l[-1] <= 0:
+                            l[-1] -= 1
+                        else:
+                            l.append(-1)
+                        y = True
+                # print(i, j, x, y)
+                if x and y:
+                    # print(i, j, l)
+                    for n in range(len(l)):
+                        ans = max(ans, abs(l[n]) - 1)
+                    curr = 0
+                    flag = True
+                    for o in l:
+                        if curr + o < 0:
+                            curr = 0
+                            flag = True
+                        elif curr == 0:
+                            curr += o
+                        else:
+                            curr += o
+                            flag = False
+                        if flag:
+                            ans = max(ans, curr - 1)
+                        else:
+                            ans = max(ans, curr)
+        return ans
+                    
