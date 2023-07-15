75a16afed77ecda665fdebb89f8306fc7f03ac1c
Dheeraj Gadwala <drgad24@gmail.com>
2023-07-15 17:24:11 -0400

    Time: 1024 ms (52.94%), Space: 207.4 MB (9.56%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort(key = lambda x: x[0])
+        n = len(events)
+        #print(events)
+        def search(tar):
+            l, h = 0, n - 1
+            ret = None
+            while l <= h:
+                m = (l + h) // 2
+                if events[m][0] < tar:
+                    l = m + 1
+                else:
+                    ret = m
+                    h = m - 1
+            return ret
+        
+        @cache
+        def res(pos = 0, k = k):
+            if pos == None or pos == n or k == 0:
+                return 0
+            else:
+                #print(pos)
+                return max(events[pos][2] + res(search(events[pos][1]+1), k-1), res(pos + 1, k))
+        
+        return res()
+            
+            
+        
