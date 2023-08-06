9cd4ee3f39fe6e48ba3a06c911a0cc42fcbd6b21
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-06 15:12:33 -0400

    Time: 92 ms (36.09%), Space: 20.1 MB (6.02%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -6,7 +6,7 @@ class Solution:
         def res(i = goal, j = n):
             if i == 0 and j == 0:
                 return 1
-            elif i < j or j < 1:
+            elif i == 0 or j == 0:
                 return 0
             elif j > k:
                 return (res(i-1, j-1)*(n-j+1) + res(i-1,j)*(j-k)) % mod
