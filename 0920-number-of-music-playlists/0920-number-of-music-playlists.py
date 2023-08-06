8b3c94c5d1aec20f0d4c028fc8916249bd48801b
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-06 15:08:15 -0400

    Time: 50 ms (91.73%), Space: 17.6 MB (32.33%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        mod=10**9+7
+        
+        @cache
+        def res(i = goal, j = n):
+            if i == 0 and j == 0:
+                return 1
+            elif i < j or j < 1:
+                return 0
+            elif j > k:
+                return (res(i-1, j-1)*(n-j+1) + res(i-1,j)*(j-k)) % mod
+            else:
+                return (res(i-1,j-1)*(n-j+1)) % mod
+
+        return res()
