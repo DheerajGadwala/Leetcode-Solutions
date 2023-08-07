5f5da98afa124e315a60c433165096b74adc8454
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-07 19:11:03 -0400

    Time: 61 ms (45.49%), Space: 16.2 MB (98.32%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.py
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.py
@@ -1,22 +1,8 @@
 class Solution:
     def canSplitArray(self, nums: List[int], m: int) -> bool:
-        
-        @cache
-        def checkSum(l, r):
-            sm = 0
-            for i in range(l, r+1):
-                sm += nums[i]
-            return sm >= m
-        
-        @cache
-        def res(l = 0, r = len(nums) - 1):
-            if l == r:
-                return True
-            else:
-                ret = False
-                for i in range(l, r):
-                    if (checkSum(l, i) or l == i) and (checkSum(i+1, r) or i+1 == r):
-                        ret |= (res(l, i) and res(i+1, r))
-                return ret
-        
-        return res()
+        if len(nums) < 3:
+            return True
+        max_sum = 0        
+        for i in range(1, len(nums)):                        
+            max_sum = max(max_sum, sum(nums[i-1:i+1]))                   
+        return max_sum >= m     
