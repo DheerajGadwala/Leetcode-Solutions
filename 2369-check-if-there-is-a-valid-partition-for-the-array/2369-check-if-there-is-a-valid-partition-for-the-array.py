ef53c75c7ab7feec9bedf78147f71975452dac29
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-13 11:07:21 -0400

    Time: 942 ms (80.00%), Space: 106.7 MB (25.38%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -3,7 +3,7 @@ class Solution:
         
         n = len(nums)
         
-        @lru_cache(10**4)
+        @lru_cache(10**3)
         def res(pos = 0):
             if pos == n:
                 return True
