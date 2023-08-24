6669a5e91d67641711a5198c0546eddf18d49812
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-24 16:28:54 -0400

    Time: 1470 ms (69.22%), Space: 70.3 MB (71.84%) - LeetHub

---
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximizeTheProfit(self, n: int, offers: List[List[int]]) -> int:
+        offers.sort(key = lambda x: x[1])
+        dp = [0] * (n+1)
+        j = 0
+        for i in range(n):
+            dp[i+1] = dp[i]
+            while j < len(offers) and i >= offers[j][1]:
+                dp[i+1] = max(dp[i+1], dp[offers[j][0]] + offers[j][2])
+                j+=1
+        return dp[-1]
+        
+            
+        
