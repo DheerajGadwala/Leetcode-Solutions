e2e96ef43126d168c7979ff4c5abafa6c71ee834
Dheeraj Gadwala <drgad24@gmail.com>
2023-08-25 15:03:12 -0400

    Time: 42 ms (91.84%), Space: 17.6 MB (14.46%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        @cache
+        def res(i = 0, j = 0):
+            k = i+j
+            if i == len(s1) and j == len(s2) and k == len(s3):
+                return True
+            ret = False
+            if i < len(s1) and k < len(s3) and s1[i] == s3[k]:
+                ret |= res(i+1, j)
+            if j < len(s2) and k < len(s3) and s2[j] == s3[k]:
+                ret |= res(i, j+1)
+            return ret
+        return res()
