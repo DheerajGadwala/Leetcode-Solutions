f057f6a8c5f41fe1cdfd0f8dfd60f551d1603e53
Dheeraj Gadwala <drgad24@gmail.com>
2023-09-04 17:31:54 -0400

    Time: 943 ms (52.57%), Space: 36 MB (78.09%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        mp = {0:0}
+        mp = {0:-1}
         s = 0
         for i in range(len(nums)):
             s += nums[i]
             if s % k not in mp:
-                mp[s%k] = i + 1
-            elif mp[s%k] < i:
+                mp[s%k] = i
+            elif i - mp[s%k] > 1:
                 return True
         return False
         
