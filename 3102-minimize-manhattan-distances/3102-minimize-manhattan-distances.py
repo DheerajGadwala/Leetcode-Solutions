c49a7f7deb4a5d743fc7477a4786e66fc6dab8af
Dheeraj Gadwala <drgad24@gmail.com>
2024-04-06 17:09:01 -0400

    Time: 1619 ms (38.03%), Space: 64.3 MB (62.45%) - LeetHub

---
+++ 3102-minimize-manhattan-distances/3102-minimize-manhattan-distances.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minimumDistance(self, points: List[List[int]]) -> int:
+        
+        
+        
+#         for x1, y1 in points:
+#             for x2, y2 in points:
+#                 print((x1, y1), (x2, y2), abs(y2-y1)+abs(x2-x1))
+        
+        xpy = [math.inf, -math.inf]
+        xmy = [math.inf, -math.inf]
+        xpy_val = [None, None]
+        xmy_val = [None, None]
+        for i in range(len(points)):
+            x, y = points[i]
+            if x+y < xpy[0]:
+                xpy[0] = x+y
+                xpy_val[0] = i
+            if x+y > xpy[1]:
+                xpy[1] = x+y
+                xpy_val[1] = i
+            if x-y < xmy[0]:
+                xmy[0] = x-y
+                xmy_val[0] = i
+            if x-y > xmy[1]:
+                xmy[1] = x-y
+                xmy_val[1] = i
+
+        l = xpy_val + xmy_val
+        ans = math.inf
+        for j in l:
+            xpy = [math.inf, -math.inf]
+            xmy = [math.inf, -math.inf]
+            for i in range(len(points)):
+                if j == i:
+                    continue
+                x, y = points[i]
+                xpy = [min(xpy[0], x+y), max(xpy[1], x+y)]
+                xmy = [min(xmy[0], x-y), max(xmy[1], x-y)]
+            ans = min(ans, max(xpy[1]-xpy[0], xmy[1]-xmy[0]))
+
+        return ans
+        
