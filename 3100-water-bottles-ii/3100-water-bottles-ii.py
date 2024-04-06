eb545794d3109cdd2ede75b285e53bf249ebe783
Dheeraj Gadwala <drgad24@gmail.com>
2024-04-06 15:44:00 -0400

    Time: 34 ms (70.10%), Space: 16.6 MB (11.34%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.py
+++ 3100-water-bottles-ii/3100-water-bottles-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxBottlesDrunk(self, n: int, x: int) -> int:
-        
-        def res(n=n, x=x):
-            if n < x:
-                return n
-            else:
-                return x + res(n-x+1, x+1)
-        
-        return res()
+        ans = 0
+        while n >= x:
+            ans += x
+            n -= x - 1
+            x += 1
+        return ans + n
