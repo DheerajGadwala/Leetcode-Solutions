7e79f88bb748246f4916a0085418b5b562a8b1bd
Dheeraj Gadwala <drgad24@gmail.com>
2024-08-03 16:19:13 -0400

    Time: 713 ms (5.05%), Space: 40.9 MB (5.00%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,41 +1,49 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
-        m, n = len(matrix), len(matrix[0])
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        neighbours = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        
         isValid = lambda x, y: x > -1 and y > -1 and x < m and y < n
         
-        dist = dict()
-        ans = 0
+        visited = set()
+        
+        @cache
+        def getMaxPath(curr, prev=None):
+            
+            visited.add(curr)
+            i = curr // n
+            j = curr % n
+            
+            ret = 0
+            
+            for nei in neighbours:
+                
+                to_i = i + nei[0]
+                to_j = j + nei[1]
+                
+                to = to_i * n + to_j
+                
+                if to != prev and isValid(to_i, to_j) and to not in visited and matrix[to_i][to_j] > matrix[i][j]:
+                    
+                    ret = max(ret, 1 + getMaxPath(to, curr))
+            
+            visited.remove(curr)
+            
+            return ret
+        
+        ans = 1
+        
         for i in range(m):
             for j in range(n):
-                
-                if (i, j) in dist:
-                    continue
-                    
-                dist[(i, j)] = 1
-                
-                q = [(matrix[i][j], i, j)]
-                
-                while len(q) != 0:
-                    u = heappop(q)
-                    p = [u[1], u[2]]
-                    ans = max(ans, dist[(p[0], p[1])])
-                    
-                    for d in dirs:
-                        x, y = p[0] + d[0], p[1] + d[1]
-                        if isValid(x, y) and matrix[x][y] > matrix[p[0]][p[1]]:
-                            if (x, y) not in dist:
-                                dist[(x, y)] = 1 + dist[(p[0], p[1])]
-                                heappush(q, (matrix[x][y], x, y))
-                            elif dist[(x, y)] < 1 + dist[(p[0], p[1])]:
-                                dist[(x, y)] = 1 + dist[(p[0], p[1])]
-                                heappush(q, (matrix[x][y], x, y))
-                
+                ans = max(ans, getMaxPath(i*n+j) + 1)
+        
         return ans
                     
-                    
-                    
-                    
-                
-                
+            
+            
+            
+            
+            
